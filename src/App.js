5f19b43226a48ce8e06b1d37c89bb6b6843ebfb6
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-17 16:11:06 +0530

    Epi - 12 - Context Api - Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Blogs from "./components/Blogs";
+import Header from "./components/Header";
+import Pagination from "./components/Pagination";
+
+export default function App() {
+  return <div>
+    <Header />
+    <Blogs />
+    <Pagination />
+  </div>;
+}
