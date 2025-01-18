a8f315bbf07a6d065f25d15a4cdf7798ae599501
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-18 17:04:37 +0530

    s

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Blogs from "./components/Blogs";
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import { AppContext } from "./context/AppContext";
+import "./App.css"
 
 export default function App() {
   const {fetchBlogPosts} = useContext(AppContext);
