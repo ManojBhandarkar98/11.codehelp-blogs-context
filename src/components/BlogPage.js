4c31ff6564a947c24040a7b925f7fcf37f5d14c6
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-20 12:31:00 +0530

    Epi - 13 - Blogs Context - BlogPage component -fetchrelatedblog function handle blogID call get data about blog, relatedblog

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { useLocation, useNavigation } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import { baseUrl } from '../baseUrl';
 
 const BlogPage = () => {
-   
-  return (
-    <div>
-        
-    </div>
-  )
+    const [blog, setBlog] = useState(null);
+    const [relatedBlogs, setRelatedBlogs] = useState([]);
+    const location = useLocation();
+    const navigation = useNavigation();
+    const [setLoading, loading] = useContext(AppContext);
+
+    const blogId = location.pathname.split("/").at(-1);
+
+    async function fetchRelatedBlogs() {
+        setLoading(true);
+        let url = `${baseUrl}?blogId=${blogId}`;
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            setBlog(data.blog);
+            setRelatedBlogs(data.relatedBlogs);
+        }
+        catch(error){
+            console.log("Error in blogid call");
+            setBlog(null);
+            setRelatedBlogs([]);
+        }
+        setLoading(false);
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default BlogPage
